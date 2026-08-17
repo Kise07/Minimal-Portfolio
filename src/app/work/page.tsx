cd282c0ab26cf84f7d290477292e32de5b700835
kise <kaiazmi1412@gmail.com>
2026-08-17 15:02:20 +0530

    feat: LayoutId effect [Pattern]
    - reactCompiler: true => false worked the Animation.
    - LayouId for Animations

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,9 +1,3 @@
-import { NavSmooth } from "@/components/work/nav-smooth";
-
 export default function Work() {
-  return (
-    <div>
-      <NavSmooth />
-    </div>
-  );
+  return <div></div>;
 }
--- src/components/work/nav-smooth.tsx
+++ src/components/work/nav-smooth.tsx
@@ -1,6 +1,6 @@
 "use client";
-import Link from "next/link";
 import { motion } from "motion/react";
+import Link from "next/link";
 import { useState } from "react";
 
 export const NavSmooth = () => {
