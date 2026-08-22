471718aa84c191aa24619aa3aeb818d96b11246d
kise <kaiazmi1412@gmail.com>
2026-08-22 10:30:02 +0530

    chore: dashboard in progress...
    - charts area
    - site header

---
+++ src/components/dashboard/dashboard-1/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Container } from "@/components/core/container";
+
+export default function Page() {
+  return (
+    <Container className="flex items-center justify-center mt-32">
+      dashboard-1
+    </Container>
+  );
+}
---
+++ src/components/dashboard/dashboard-1/components/chart-area-interactive.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export function ChartAreaInteractive() {
+  const isMobile = useIsMobile();
+  const [timeRange, setTimeRange] = useState("90d");
+  useEffect(() => {
+    if (isMobile) {
+      setTimeRange("7d");
+    }
+  }, [isMobile]);
+
+  const filteredData = chartData.filter((item) => {
+    const date = new Date(item.date);
+    const referenceDate = new Date("2024-06-30");
+    let daysToSubtract = 90;
+    if (timeRange === "30d") {
+      daysToSubtract = 30;
+    } else if (timeRange === "7d") {
+      daysToSubtract = 7;
+    }
+    const startDate = new Date(referenceDate);
+    startDate.setDate(startDate.getDate() - daysToSubtract);
+    return date >= startDate;
+  });
+
+  return <div></div>;
+}
