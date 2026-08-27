0f54888d4299130eac8e25c6aacc802813c32d83
kise <kaiazmi1412@gmail.com>
2026-08-27 14:43:54 +0530

    feat: animated-orbit effect [New Pattern]
    - keyframe, svg animation in -> globals.css
    - relative, absolute, inset used cases more
    - skeletonCard for more cards showcase

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,10 +1,12 @@
 import { Container } from "@/components/core/container";
-import { SkeletonOne } from "@/components/work/features/skeleton-2/first";
+import { ModeToggle } from "@/components/ui/theme-toggle";
+import { SkeletonTwo } from "@/components/work/features/skeleton-2/second";
 
 export default function Work() {
   return (
-    <Container className="flex items-center justify-center m-auto mt-32">
-      <SkeletonOne />
+    <Container className="overflow-hidden flex items-center bg-neutral-300 rounded-3xl p-48 justify-center mt-32 ring-2 ring-black/80">
+      <ModeToggle className="fixed top-10 right-10" />
+      <SkeletonTwo />
     </Container>
   );
 }
