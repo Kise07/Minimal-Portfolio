5da65a8c5adb7f16d6ca89827ddd705d0f82a597
kise <kaiazmi1412@gmail.com>
2026-09-17 16:13:51 +0530

    chore: motion effect

--- src/components/work/minimal/intro-lines.tsx
+++ src/components/work/minimal/intro-lines.tsx
@@ -6,28 +6,29 @@ import { Greetings, Role } from "./text-loop";
 export const IntroLines = ({ className }: { className?: string }) => {
   return (
     <motion.div
-      initial={{ opacity: 0, x: -10 }}
-      whileInView={{ opacity: 1, x: 0 }}
-      transition={{ duration: 0.5, delay: 0.2 }}
+      initial={{ opacity: 0, filter: "blur(10px)", y: 10 }}
+      whileInView={{ opacity: 1, filter: "blur(0px)", y: 0 }}
+      transition={{ duration: 0.3, ease: "easeOut", delay: 0.2 }}
+      viewport={{ once: true }}
       className={"text-sm text-neutral-600 dark:text-neutral-400 " + className}
     >
       {" "}
       <Greetings className="mb-4" />
       <div>
-        <span>
+        <span className="tracking-wide text-pretty md:text-balance">
           I&apos;m a{" "}
           <Role className="mx-2 rounded-md px-2 py-0.5 text-sm shadow-sm ring-1 shadow-black/50 ring-black/10 sm:order-last sm:mx-0 sm:mb-0 dark:shadow-white/50 dark:ring-white/10" />{" "}
           .
-          <br />
-          I enjoy taking an idea, breaking it down, and turning it into
+          <br />I enjoy taking an idea, breaking it down, and turning it into
           something people can use.
-          <br />
-          Most of my time goes into building for the web, learning new
-          technologies, and experimenting with interfaces, motion, and
-          interactions.
-          <br />
-          This is a collection of the things I&apos;ve built, learned, and
-          enjoyed along the way.
+          <p>
+            Most of my time goes into building for the web, learning new
+            technologies, and experimenting with interfaces, motion, and
+            interactions.
+            <br />
+            This is a collection of the things I&apos;ve built, learned, and
+            enjoyed along the way.
+          </p>
         </span>
       </div>
     </motion.div>
